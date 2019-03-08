3a38a0497b936461a9899fbe06cd1ceb82228945
Rafa Mel <melrcm@gmx.net>
2019-03-08 15:14:51 +0100

    chore: updates setup & dependencies

---
+++ typedoc.js
@@ -0,0 +1,17 @@
+const pkg = require('./package.json');
+const path = require('path');
+
+module.exports = {
+  name: `${pkg.name} ${pkg.version}`,
+  mode: 'file',
+  module: 'system',
+  theme: 'default',
+  includeDeclarations: true,
+  excludePrivate: true,
+  excludeProtected: true,
+  excludeExternals: true,
+  excludePrivate: true,
+  excludeNotExported: false,
+  readme: path.join(__dirname, 'README.md'),
+  exclude: ['**/internal/**/*.ts']
+};
